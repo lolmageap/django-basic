9fd952c7970836e9484352da6db58e785aa75036
lolmageap <ekxk1234@naver.com>
2025-02-17 23:42:43 +0900

    implement sign up model

--- test_project/serializers.py
+++ test_project/serializers.py
@@ -1,9 +1,35 @@
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
-from .model import Todos
+from .model import Todos, Users
 
 
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todos
         fields = '__all__'
+
+
+class PasswordValidator:
+    @staticmethod
+    def validate(self):
+        if self['password'] != self['password_confirm']:
+            raise serializers.ValidationError('Password does not match')
+        return self
+
+
+def create(validated_data):
+    validated_data.pop("password_confirm")
+    validated_data["password"] = make_password(validated_data["password"])
+    return Users.objects.create(**validated_data)
+
+
+class SingUpSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=150)
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=125)
+    password_confirm = serializers.CharField(max_length=125)
+    __validate__ = PasswordValidator.validate
+    __create__ = create
+
+
--- test_project/views.py
+++ test_project/views.py
@@ -1,7 +1,7 @@
 from rest_framework import generics
 
 from .model import Todos
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer, SingUpSerializer
 
 
 def perform_create(serializer: TodoSerializer) -> None:
@@ -17,3 +17,12 @@ class GetTodoView(generics.RetrieveAPIView):
     queryset = Todos.objects.all()
     serializer_class = TodoSerializer
     lookup_field = 'pk'
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = SingUpSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
