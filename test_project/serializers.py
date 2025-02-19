8770acd354743709708e7aaf6698bf6bd4e33d97
lolmageap <ekxk1234@naver.com>
2025-02-19 22:57:09 +0900

    implement sign up and create todo, find todo serializers

--- test_project/serializers.py
+++ test_project/serializers.py
@@ -7,7 +7,33 @@ from .model import Todos, Users
 class TodoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Todos
-        fields = '__all__'
+        fields = 'title', 'description', 'is_completed', 'user'
+
+        @staticmethod
+        def validate(data):
+            title = data.get('title', '')
+            if not (3 <= len(title) <= 50):
+                raise serializers.ValidationError({'title': 'Title must be between 3 and 50 characters.'})
+            return data
+
+
+class FindOneTodoResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todos
+        fields = ('id', 'title', 'description', 'is_completed', 'user', 'created_at')
+
+
+class FindAllTodoRequestSerializer(serializers.Serializer):
+    user = serializers.IntegerField(required=True)
+    start_date = serializers.DateField(required=False)
+    end_date = serializers.DateField(required=False)
+    is_completed = serializers.BooleanField(required=False)
+
+
+class FindAllTodoResponseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Todos
+        fields = ('id', 'title', 'description', 'is_completed', 'user', 'created_at')
 
 
 class PasswordValidator:
@@ -18,19 +44,21 @@ class PasswordValidator:
         return self
 
 
-def create(validated_data):
-    validated_data.pop("password_confirm")
-    validated_data["password"] = make_password(validated_data["password"])
-    return Users.objects.create(**validated_data)
-
-
-class SingUpSerializer(serializers.Serializer):
+class SignUpSerializer(serializers.Serializer):
     name = serializers.CharField(max_length=150)
     email = serializers.EmailField()
     password = serializers.CharField(max_length=125)
     password_confirm = serializers.CharField(max_length=125)
     __validate__ = PasswordValidator.validate
-    __create__ = create
+
+    def validate(self, data):
+        return self.__validate__(data)
+
+    def create(self, validated_data):
+        validated_data.pop("password_confirm")
+        validated_data["password"] = make_password(validated_data["password"])
+        return Users.objects.create(**validated_data)
+
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
