4d3b2f1483cdbc4f84f5b957666bf9bf0574e317
lolmageap <ekxk1234@naver.com>
2025-02-19 22:45:25 +0900

    rename to find one todo view

--- test_project/urls.py
+++ test_project/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import GetTodoView, CreateTodoView, LoginView, SignUpView
+from .views import FindOneTodoView, CreateTodoView, LoginView, SignUpView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todos/<long:pk>/', GetTodoView.as_view(), name='get_todo'),
+    path('todos/<long:pk>/', FindOneTodoView.as_view(), name='get_todo'),
     path('todos/', CreateTodoView.as_view(), name='create_todo'),
     path('sign-up/', SignUpView.as_view(), name='sign_up'),
     path('login/', LoginView.as_view(), name='login'),
