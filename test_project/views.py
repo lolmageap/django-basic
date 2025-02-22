b55da0d20e0fcffbfe120c10972a6bccb913e2ca
lolmageap <ekxk1234@naver.com>
2025-02-22 23:58:15 +0900

    change view classes

--- test_project/views.py
+++ test_project/views.py
@@ -1,24 +1,23 @@
 from django.contrib.auth import authenticate
-from rest_framework import generics
 from rest_framework import status
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.generics import CreateAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-
 from .model import Todos
 from .serializers import CreateTodoSerializer, LoginSerializer, SignUpSerializer, FindOneTodoResponseSerializer, \
     FindAllTodoResponseSerializer, FindAllTodoRequestSerializer
 
 
-class CreateTodoView(generics.CreateAPIView):
+class CreateTodoView(CreateAPIView):
     serializer_class = CreateTodoSerializer
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+    def perform_create(self, serializer):
+        serializer = self.get_serializer(data=self.request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({"message": "Todo created successfully"}, status=status.HTTP_201_CREATED)
@@ -39,7 +38,7 @@ class FindOneTodoView(APIView):
         return Response(response_serializer.data, status=status.HTTP_200_OK)
 
 
-class FindAllTodoView(generics.ListAPIView):
+class FindAllTodoView(ListAPIView):
     serializer_class = FindAllTodoResponseSerializer
     queryset = Todos.objects.all()
     authentication_classes = [TokenAuthentication]
@@ -68,28 +67,28 @@ class FindAllTodoView(generics.ListAPIView):
         return queryset
 
 
-class SignUpView(generics.GenericAPIView):
+class SignUpView(CreateAPIView):
     serializer_class = SignUpSerializer
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+    def perform_create(self, serializer):
+        serializer = self.get_serializer(data=self.request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({"message": "User created successfully"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-class LoginView(generics.GenericAPIView):
+class LoginView(CreateAPIView):
     serializer_class = LoginSerializer
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.get_serializer(data=request.data)
+    def perform_create(self, serializer):
+        serializer = self.get_serializer(data=self.request.data)
         serializer.is_valid(raise_exception=True)
 
         username = serializer.validated_data.get("username")
         password = serializer.validated_data.get("password")
 
-        user = authenticate(request, username=username, password=password)
+        user = authenticate(self.request, username=username, password=password)
 
         if user is not None:
             refresh = RefreshToken.for_user(user)
@@ -111,11 +110,12 @@ class LoginView(generics.GenericAPIView):
         else:
             return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
 
-class LogoutView(APIView):
+
+class LogoutView(CreateAPIView):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
-    def post(self, request):
+    def perform_create(self, serializer):
         response = Response({"message": "Logout successful"}, status=status.HTTP_200_OK)
         response.headers["Authorization"] = ""
         response.delete_cookie("refresh_token")
