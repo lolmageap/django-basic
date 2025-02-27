0d043ff71f85e4a04de521a4a10c496eabe0504e
lolmageap <ekxk1234@naver.com>
2025-02-27 23:25:31 +0900

    add AuthenticationFailedException for custom authentication error handling

--- myapp/custom_exception.py
+++ myapp/custom_exception.py
@@ -1,6 +1,15 @@
 class NotFoundException(Exception):
-    def __init__(self, message):
+    def __init__(self, message, *args):
+        super().__init__(*args)
         self.message = message
         self.status_code = 404
+    def __str__(self):
+        return self.message
+
+class AuthenticationFailedException(Exception):
+    def __init__(self, message, *args):
+        super().__init__(*args)
+        self.message = message
+        self.status_code = 401
     def __str__(self):
         return self.message
