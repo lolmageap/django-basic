e935eeebaf462376b33903cf12b06024b92efb3b
lolmageap <ekxk1234@naver.com>
2025-02-28 19:18:42 +0900

    refactor RoleBasedPermission to streamline token validation process

--- common/permissions.py
+++ common/permissions.py
@@ -4,16 +4,12 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 class RoleBasedPermission(BasePermission):
     def has_permission(self, request, view) -> bool:
         auth = JWTAuthentication()
-
         auth_header = request.headers.get("Authorization")
         if not auth_header: return False
 
-        try:
-            token = auth_header.strip("Bearer ")
-            validated_token = auth.get_validated_token(token)
-        except Exception: return False
+        token = auth_header.strip("Bearer").strip()
+        validated_token = auth.get_validated_token(token)
 
         user_roles = validated_token.get("roles", [])
         required_roles = getattr(view, "roles", [])
-
         return any(role in user_roles for role in required_roles)
