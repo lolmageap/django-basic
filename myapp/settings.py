a39871ab1657d19a8325da19742b2b34f5013f1d
lolmageap <ekxk1234@naver.com>
2025-03-02 14:26:04 +0900

    add corsheaders to settings.py and enable CORS for all origins

--- myapp/settings.py
+++ myapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_filters',
+    'corsheaders',
     'rest_framework',
     'rest_framework.authtoken',
     'rest_framework_simplejwt',
@@ -131,6 +132,13 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'myapp.wsgi.application'
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+# CORS_ALLOWED_ORIGINS = [
+#     "http://example.com",
+#     "http://localhost:3000",
+# ]
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
