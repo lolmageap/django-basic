2266247c2dbde19765509d8c70b5a395bb076a9b
lolmageap <ekxk1234@naver.com>
2025-03-06 23:38:09 +0900

    add key-based filtering to get_queryset for improved pagination

--- common/infinitescroll.py
+++ common/infinitescroll.py
@@ -5,9 +5,14 @@ class InfiniteScrollPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 100
+    key = 'key'
+    order = 'ASC'
 
     def get_queryset(self, request, queryset):
-        start_after = request.query_params.get('start_after')
-        if start_after:
-            queryset = queryset.filter(id__gt=start_after).order_by('id')
-        return queryset
+        if self.key not in request.query_params:
+            return queryset
+        key: int = request.query_params[self.key]
+        order = self.order
+        if order.lower() == 'asc':
+            return queryset.filter(id__gt=key)
+        return queryset.filter(id__lt=key)
