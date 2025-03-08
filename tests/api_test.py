1e7b53f8459488b4554ac025809493af224d35c3
lolmageap <ekxk1234@naver.com>
2025-03-08 23:56:06 +0900

    add test for sign-up v3 using SignUpRequestV2 model

--- tests/api_test.py
+++ tests/api_test.py
@@ -4,7 +4,7 @@ import pytest
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
-from users.request import SignUpRequest
+from users.request import SignUpRequest, SignUpRequestV2
 
 @pytest.fixture
 def api_client():
@@ -33,4 +33,16 @@ def test_signup_v2(api_client):
     )
 
     response = api_client.post(reverse('sign-up-v2'), asdict(request), format='json')
+    assert response.status_code == status.HTTP_201_CREATED
+
+@pytest.mark.django_db
+def test_signup_v3(api_client):
+    request = SignUpRequestV2(
+        name="cheolhee",
+        email="ekxk1234@naver.com",
+        password="String1234!",
+        password_confirm="String1234!"
+    )
+
+    response = api_client.post(reverse('sign-up-v2'), request.model_dump(), format='json')
     assert response.status_code == status.HTTP_201_CREATED
