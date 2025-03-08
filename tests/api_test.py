bb4f4d9a448bb72d0ef9d9b6faffe09aea789e57
lolmageap <ekxk1234@naver.com>
2025-03-08 23:49:00 +0900

    add API tests for user sign-up and sign-up v2 endpoints

---
+++ tests/api_test.py
@@ -0,0 +1,36 @@
+from dataclasses import asdict
+
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from users.request import SignUpRequest
+
+@pytest.fixture
+def api_client():
+    return APIClient()
+
+
+@pytest.mark.django_db
+def test_signup(api_client):
+    request_data = {
+        "name": "cheolhee",
+        "email": "ekxk1234@naver.com",
+        "password": "String1234!",
+        "password_confirm": "String1234!"
+    }
+    response = api_client.post(reverse('sign-up'), request_data, format='json')
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_signup_v2(api_client):
+    request = SignUpRequest(
+        name="cheolhee",
+        email="ekxk1234@naver.com",
+        password="String1234!",
+        password_confirm="String1234!"
+    )
+
+    response = api_client.post(reverse('sign-up-v2'), asdict(request), format='json')
+    assert response.status_code == status.HTTP_201_CREATED
