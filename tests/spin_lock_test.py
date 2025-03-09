62bc9bc255fa510688d9d30587d1c0f1d7e99115
lolmageap <ekxk1234@naver.com>
2025-03-09 22:51:52 +0900

    add tests for SpinLock functionality with multiple threads

--- tests/spin_lock_test.py
+++ tests/spin_lock_test.py
@@ -1,4 +1,5 @@
 import time
+
 from concurrent.futures import ThreadPoolExecutor
 
 from common.spin_lock import SpinLock
@@ -6,19 +7,66 @@ from common.spin_lock import SpinLock
 value = 0
 
 
-@SpinLock("test1")
-def plus_one() -> int:
+def no_lock_plus_one() -> int:
     global value
     temp = value
     time.sleep(0.1)
     value = temp + 1
-    print(value)
     return value
 
 
-def test_spin_lock():
+def plus_one_with_spin_lock() -> int:
+    with SpinLock("test1"):
+        global value
+        temp = value
+        time.sleep(0.1)
+        value = temp + 1
+        return value
+
+
+@SpinLock("test1")
+def plus_one_with_spin_lock_v2() -> int:
+    global value
+    temp = value
+    time.sleep(0.1)
+    value = temp + 1
+    return value
+
+
+def test_no_lock():
+    global value
+    value = 0
+
     num_threads = 10
     with ThreadPoolExecutor(max_workers=num_threads) as executor:
-        futures = [executor.submit(plus_one) for _ in range(num_threads)]
+        futures = [executor.submit(no_lock_plus_one) for _ in range(num_threads)]
         for future in futures:
             future.result()
+
+    assert value < num_threads
+
+
+def test_spin_lock():
+    global value
+    value = 0
+
+    num_threads = 10
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        futures = [executor.submit(plus_one_with_spin_lock) for _ in range(num_threads)]
+        for future in futures:
+            future.result()
+
+    assert value == num_threads
+
+
+def test_spin_lock_v2():
+    global value
+    value = 0
+
+    num_threads = 10
+    with ThreadPoolExecutor(max_workers=num_threads) as executor:
+        futures = [executor.submit(plus_one_with_spin_lock_v2) for _ in range(num_threads)]
+        for future in futures:
+            future.result()
+
+    assert value == num_threads
